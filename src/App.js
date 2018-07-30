7fd2d76a41383f8888dc1991419d2cf4ec03068f
Jay Khatri <khatri.florida@gmail.com>
2018-07-29 21:04:47 -0700

    initial layout sorta working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,70 @@
-import React from 'react'
-import { hot } from 'react-hot-loader'
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import { Flex } from 'grid-styled';
+import { Input, Button, TextField } from '@material-ui/core';
 
-import './app.css'
+import {
+  Background,
+  RegistrationText,
+  SponsorText,
+  Title,
+  SubtitleOne,
+  SubtitleTwo,
+} from './app_styles.js';
+
+import './app.css';
 
 const App = () => (
-    <div>
-	New tamuhack site! This is good. Update. update
-    </div>
-)
+  <Background>
+    <Flex
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      css={{ width: '100vw', height: '100vh' }}
+    >
+      <div>
+        <Flex>
+          <Title>tamuhack</Title>
+        </Flex>
+        <Flex>
+          <Flex>
+            <SubtitleOne>JAN. 26-27, 2019 &nbsp;</SubtitleOne>
+          </Flex>
+          <Flex>
+            <SubtitleTwo> • TEXAS A&M UNIVERSITY</SubtitleTwo>
+          </Flex>
+        </Flex>
+        <Flex>
+          <RegistrationText>Registration opens October 24th, 2018.</RegistrationText>
+        </Flex>
 
-export default hot(module)(App)
+        <Flex flexDirection="row">
+          <Flex alignItems="center" justifyContent="center" css={{ width: '80%' }}>
+            <TextField
+              fullWidth
+              label="Sign up for updates!"
+              placeholder="myemail@gmail.com"
+              margin="normal"
+            />
+          </Flex>
+          <Flex
+            css={{ marginTop: '28px', height: '36px' }}
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Button>Submit</Button>
+          </Flex>
+        </Flex>
+
+        <Flex>
+          <SponsorText>
+            Interested in
+            <a href="https://app.tamuhack.com"> sponsoring?</a>
+          </SponsorText>
+        </Flex>
+      </div>
+    </Flex>
+  </Background>
+);
+
+export default hot(module)(App);
---
+++ src/app_styles.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const Title = styled.p`
+  font-size: 50px;
+  font-weight: 600;
+  line-height: 0;
+  margin-bottom: 50px;
+`;
+
+export const Background = styled.p`
+  background: linear-gradient(#91A9CA, #FDF4E9);
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding:0;
+`;
+
+export const SubtitleOne = styled.p`
+  font-size: 15px;
+  font-weight: 600;
+  margin-top: 0;
+`;
+
+export const SubtitleTwo = styled.p`
+  font-size: 15px;
+  font-weight: 500;
+  margin-top: 0;
+`;
+
+export const SponsorText = styled.p`
+  font-size: 15px;
+  font-weight: 500;
+`;
+
+export const RegistrationText = styled.p`
+  margin-top: 40px;
+  margin-bottom: 0px;
+  font-size: 15px;
+  font-weight: 600;
+`;
